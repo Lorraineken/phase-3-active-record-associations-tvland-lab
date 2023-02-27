16d65d1732e4a2300564ff3e4060da4cf8bee0ba
Lorraineken <lorrainekupa@gmail.com>
2023-02-27 11:52:21 +0300

    Completed Assignment

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
 class Show < ActiveRecord::Base
-  
+belongs_to :network
+has_many :characters
+has_many :actors, through: :characters
+
+ def actors_list
+    actors_arr =[]
+    self.characters.map do |tr|
+        actors_arr << "#{tr.actor.first_name} #{tr.actor.last_name}"
+    end
+    actors_arr
+ end
+
 end
